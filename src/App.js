bfed7d59deb9e5a83f1d6c4705ff7feec481b586
Caroline Miller <caroline.miller@ymail.com>
2021-06-28 14:59:32 -0500

    reorganized and linked files to one another, ready for code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Recordings from "./components/pages/Recordings";
-import Contact from "./components/pages/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Recordings from "./pages/Recordings/Recordings";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
+        <Navbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/recordings" component={Recordings} />
