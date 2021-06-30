96be1c73152588f3077d8e6583f8c02f88658c29
Caroline Miller <caroline.miller@ymail.com>
2021-06-30 16:56:38 -0500

    fixed homepage render

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/trevor-king-bassoon" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/recordings" component={Recordings} />
         <Route path="/contact" component={Contact} />
