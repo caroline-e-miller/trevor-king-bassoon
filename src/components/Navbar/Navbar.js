a577d51dca9ab63f8e33b1ec394f97c2c0a5919d
Caroline Miller <caroline.miller@ymail.com>
2021-07-21 18:00:12 -0500

    fixed hung code, navbar appearance

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Navbar.css";
 
-function Navbar() {
+function Navigator() {
     const location = useLocation();
 
     return (
@@ -18,46 +18,46 @@ function Navbar() {
         //         </Nav>
         //     </Container>
         // </Navbar>
-        // <Navbar collapseOnSelect expand="sm" variant="light" bg="light">
-        //     <Navbar.Brand href="/">T/K</Navbar.Brand>
-        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //     <Navbar.Collapse id="basic-navbar-nav">
-        //         <Nav className="mr-auto">
-        <ul className="nav nav-bar">
-            <li className="nav-item">
-                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-                    Home
+        <Navbar collapseOnSelect expand="sm" variant="light" bg="light">
+            <Navbar.Brand href="/">T/K</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <ul className="nav nav-bar">
+                        <li className="nav-item">
+                            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                                Home
         </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/about"
-                    className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-                >
-                    About
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/about"
+                                className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+                            >
+                                About
         </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/contact"
-                    className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
-                >
-                    Contact
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/contact"
+                                className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
+                            >
+                                Contact
         </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/recordings"
-                    className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-                >
-                    Recordings
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/recordings"
+                                className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+                            >
+                                Recordings
         </Link>
-            </li>
-        </ul>
-        //         </Nav>
-        //     </Navbar.Collapse>
-        // </Navbar >
+                        </li>
+                    </ul>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar >
     );
 }
 
-export default Navbar;
+export default Navigator;
