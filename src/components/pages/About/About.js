969bd14651d413e9a0f6a92f5a158c49039f9e51
Caroline Miller <caroline.miller@ymail.com>
2021-08-05 16:22:48 -0500

    replaced jumbo with card and added shadow

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Jumbotron, Container, Row, Image } from 'react-bootstrap';
+import { Card, Container, Row, Image } from 'react-bootstrap';
 import "./About.css"
 
 function About() {
     return (
         <Container>
             <Row>
-                <Jumbotron>
-                    <Container>
 
-                        <h1>About Trevor</h1>
-                        <p>
-                            Trevor King grew up in Ann Arbor Michigan, and attended the University of Michigan for his undergraduate degree. After meeting his partner, Caroline Miller, at the University of Texas at Austin, they decided together to move back to Ann Arbor. It's an awesome place.
+                <Card>
+
+                    <h1>About Trevor</h1>
+                    <p>
+                        Trevor King grew up in Ann Arbor Michigan, and attended the University of Michigan for his undergraduate degree. After meeting his partner, Caroline Miller, at the University of Texas at Austin, they decided together to move back to Ann Arbor. It's an awesome place.
     </p>
-                    </Container>
-                </Jumbotron>
+                </Card>
+
             </Row>
 
             <Row>
